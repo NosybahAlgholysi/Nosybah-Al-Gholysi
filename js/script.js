5138536f67a2236735cb9184d9c43079a354956a
Nosybah Algholysi <94728815+NosybahAlgholysi@users.noreply.github.com>
2022-10-14 22:58:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* typing aninmation */
+
+var typed =new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphics Designer","English Trainer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
